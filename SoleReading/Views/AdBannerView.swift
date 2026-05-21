a48cbf22c24eb16fe838c0bb84fb125f3b78b426
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-05-22 06:30:33 +0900

    Revert "Remove AdMob SDK (iPad crash fix), fix CI signing"
    This reverts commit ae076b8213eecd3894d782df40bf23293dc900c8.

--- SoleReading/Views/AdBannerView.swift
+++ SoleReading/Views/AdBannerView.swift
@@ -1,7 +1,18 @@
 import SwiftUI
+import GoogleMobileAds
 
-struct AdBannerView: View {
-    var body: some View {
-        EmptyView()
+struct AdBannerView: UIViewRepresentable {
+    let adUnitID = "ca-app-pub-9404799280370656/7472066021"
+
+    func makeUIView(context: Context) -> GADBannerView {
+        let banner = GADBannerView(adSize: GADAdSizeBanner)
+        banner.adUnitID = adUnitID
+        banner.rootViewController = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .first?.keyWindow?.rootViewController
+        banner.load(GADRequest())
+        return banner
     }
+
+    func updateUIView(_ uiView: GADBannerView, context: Context) {}
 }
